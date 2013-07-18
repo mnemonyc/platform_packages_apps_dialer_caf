bd40fc5ffa20a8a1cf19f71662981c94593d98db
jianzhou <jianzhou@codeaurora.org>
2013-07-18 00:03:41 -0400

    Contacts: add speed dial feature
    allow user to set speed dial number from 2~9,when long click
    these numbers,it will pop up a screen to set speed number
    or dial out directly if you have been set it.
    
    Change-Id: I228a74e2cbe08bc1d0bd66fcdda28975d26bb4e3

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -86,6 +86,8 @@ import com.android.contacts.common.util.StopWatch;
 import com.android.dialer.DialtactsActivity;
 import com.android.dialer.R;
 import com.android.dialer.SpecialCharSequenceMgr;
+import com.android.dialer.SpeedDialUtils;
+import com.android.dialer.SpeedDialListActivity;
 import com.android.dialer.interactions.PhoneNumberInteraction;
 import com.android.dialer.util.OrientationUtil;
 import com.android.internal.telephony.ITelephony;
@@ -574,6 +576,11 @@ public class DialpadFragment extends Fragment
                 R.id.six, R.id.seven, R.id.eight, R.id.nine, R.id.zero, R.id.star, R.id.pound};
         for (int id : buttonIds) {
             ((DialpadImageButton) fragmentView.findViewById(id)).setOnPressedListener(this);
+            if((id != R.id.one) && (id != R.id.zero) && (id != R.id.star)
+                && (id != R.id.pound) ){
+                ((DialpadImageButton) fragmentView.findViewById(id))
+                .setOnLongClickListener(this);
+            }
         }
 
         // Long-pressing one button will initiate Voicemail.
@@ -1044,6 +1051,20 @@ public class DialpadFragment extends Fragment
 
                 return true;
             }
+            case R.id.two:
+            case R.id.three:
+            case R.id.four:
+            case R.id.five:
+            case R.id.six:
+            case R.id.seven:
+            case R.id.eight:
+            case R.id.nine:
+                if ((mDigits.length() == 1)) {
+                    removePreviousDigitIfPossible();
+                    callSpeedNumber(id);
+                    return true;
+                }
+                return false;
             case R.id.digits: {
                 // Right now EditText does not show the "paste" option when cursor is not visible.
                 // To show that, make the cursor visible, and return false, letting the EditText
@@ -1809,6 +1830,66 @@ public class DialpadFragment extends Fragment
             // caching process to happen.
             mSmartDialCache.cacheIfNeeded(false);
         }
+    }
+
+    private void callSpeedNumber(int id) {
+        SpeedDialUtils speedDialUtils = new SpeedDialUtils(getActivity());
+        int numId = 0;
+        String speedNumber;
+        switch(id) {
+        case R.id.two:
+            numId = speedDialUtils.NUM_TWO;
+            break;
+        case R.id.three:
+            numId = speedDialUtils.NUM_THREE;
+            break;
+        case R.id.four:
+            numId = speedDialUtils.NUM_FOUR;
+            break;
+        case R.id.five:
+            numId = speedDialUtils.NUM_FIVE;
+            break;
+        case R.id.six:
+            numId = speedDialUtils.NUM_SIX;
+            break;
+        case R.id.seven:
+            numId = speedDialUtils.NUM_SEVEN;
+            break;
+        case R.id.eight:
+            numId = speedDialUtils.NUM_EIGHT;
+            break;
+        case R.id.nine:
+            numId = speedDialUtils.NUM_NINE;
+            break;
+        }
+        speedNumber = speedDialUtils.getContactDataNumber(numId);
+        if (speedNumber == null || speedNumber.length() == 0) {
+            showNoSpeedNumberDialog(numId);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL_PRIVILEGED);
+            intent.setData(Uri.fromParts("tel", speedNumber, null));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            getActivity().finish();
+        }
+    }
+    private void showNoSpeedNumberDialog(int numId) {
+        //because numId start from 0, but numKey from 2 that represent num 2-9, so here add two
+        int numKey = numId + 2;
+        String dialogTxt = getString(R.string.is_set_speed, String.valueOf(numKey));
+        final Activity thisActivity = getActivity();
+        new AlertDialog.Builder(thisActivity).setTitle(R.string.dialog_title).setMessage(dialogTxt)
+          .setPositiveButton(android.R.string.ok,
+                 new DialogInterface.OnClickListener() {
+                     @Override
+                     public void onClick(DialogInterface dialog, int which) {
+                         // TODO Auto-generated method stub
+                         //go to speed dial setting screen to set speed dial number.
+                         Intent intent = new Intent(thisActivity, SpeedDialListActivity.class);
+                         startActivity(intent);
+                     }
+                }).setNegativeButton(android.R.string.cancel,null)
+                .show();
     }
 
     private String mLastDigitsForSmartDial;
