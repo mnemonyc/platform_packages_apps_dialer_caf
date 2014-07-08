573281bce62d3a3afb69676fee8346770c8ac7bf
kaiyiz <kaiyiz@codeaurora.org>
2014-07-08 11:11:01 +0800

    Dialer: Don't call finish when calling from the dialpad
    In DSDS or DSDA mode, if there is at least one sim card in phone,
    MO a call through the call button in DialpadFragment,
    it will call getActivity().finish() to finish the DialtactsActivity,
    so when end the call, it will goes to home screen.
    
    Don't call finish when calling from the dialpad.
    
    CRs-Fixed: 685376
    
    Change-Id: I90b06e6594cb479fa49e1e2922a87f67c9f74dd5

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -1355,8 +1355,7 @@ public class DialpadFragment extends Fragment
                                 ((DialtactsActivity) getActivity()).getCallOrigin() : null));
                 intent.putExtra(MSimConstants.SUBSCRIPTION_KEY, subscription);
                 startActivity(intent);
-                mClearDigitsOnStop = true;
-                getActivity().finish();
+                hideAndClearDialpad(false);
             }
         }
     }
