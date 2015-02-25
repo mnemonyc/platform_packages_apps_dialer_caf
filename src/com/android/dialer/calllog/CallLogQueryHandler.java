fe1a201f398f3c335864b31a08abc5bb35350dfa
Yashdev Singh <yashdevs@codeaurora.org>
2015-02-25 00:57:26 -0800

    Telephony: Compilation fix for subId conversion from long to int.
    - Fix for compilation issues.
    
    Conflicts:
    	src/com/android/dialer/calllog/CallLogQueryHandler.java
    Change-Id: I689c240bc0a68827b33f20d235c7d8f47bd6f63e

--- src/com/android/dialer/calllog/CallLogQueryHandler.java
+++ src/com/android/dialer/calllog/CallLogQueryHandler.java
@@ -173,6 +173,59 @@ public class CallLogQueryHandler extends NoNullCursorAsyncQueryHandler {
                 Calls.DEFAULT_SORT_ORDER);
     }
 
+    private void fetchCalls(int token, int callType, boolean newOnly,
+            long newerThan, int slotId) {
+        // We need to check for NULL explicitly otherwise entries with where READ is NULL
+        // may not match either the query or its negation.
+        // We consider the calls that are not yet consumed (i.e. IS_READ = 0) as "new".
+        StringBuilder where = new StringBuilder();
+        List<String> selectionArgs = Lists.newArrayList();
+
+        if (newOnly) {
+            where.append(Calls.NEW);
+            where.append(" = 1");
+        }
+
+        if (callType > CALL_TYPE_ALL) {
+            if (where.length() > 0) {
+                where.append(" AND ");
+            }
+            // Add a clause to fetch only items of type voicemail.
+            where.append(String.format("(%s = ?)", Calls.TYPE));
+            // Add a clause to fetch only items newer than the requested date
+            selectionArgs.add(Integer.toString(callType));
+        }
+
+        if (slotId > CALL_SIM_ALL) {
+            int[] subId = SubscriptionManager.getSubId(slotId);
+            if (subId != null && subId.length >= 1) {
+                if (where.length() > 0) {
+                    where.append(" AND ");
+                }
+                where.append(String.format("(%s = ?)", Calls.PHONE_ACCOUNT_ID));
+                selectionArgs.add(Long.toString(subId[0]));
+            }
+        }
+
+        if (newerThan > 0) {
+            if (where.length() > 0) {
+                where.append(" AND ");
+            }
+            where.append(String.format("(%s > ?)", Calls.DATE));
+            selectionArgs.add(Long.toString(newerThan));
+        }
+
+        final int limit = (mLogLimit == -1) ? NUM_LOGS_TO_DISPLAY : mLogLimit;
+        final String selection = where.length() > 0 ? where.toString() : null;
+        Uri uri = Calls.CONTENT_URI_WITH_VOICEMAIL.buildUpon()
+                .appendQueryParameter(Calls.LIMIT_PARAM_KEY, Integer.toString(limit))
+                .build();
+        startQuery(token, null, uri,
+                CallLogQuery._PROJECTION, selection, selectionArgs.toArray(EMPTY_STRING_ARRAY),
+                Calls.DEFAULT_SORT_ORDER);
+    }
+
+
     /** Cancel any pending fetch request. */
     private void cancelFetch() {
         cancelOperation(QUERY_CALLLOG_TOKEN);
