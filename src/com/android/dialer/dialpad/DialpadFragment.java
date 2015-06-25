7b4eb037874e08499ed9d30e7a5de801f405469e
Jack Yoo <jyoo@codeaurora.org>
2015-06-25 10:50:00 -0700

    Dialer: Change api reference pointer.
    Changing isVideoCallNumValid to another
    
    Change-Id: Ide9b4902d6e2edb1817817744e2a386d7a631de2

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -1782,7 +1782,9 @@ public class DialpadFragment extends AnalyticsFragment
                 return true;
             case R.id.menu_video_call:
                 final String number = mDigits.getText().toString();
-                if (!CallUtil.isVideoCallNumValid(getActivity(), number)) {
+                if (getResources().getBoolean(
+                           com.android.internal.R.bool.config_regional_number_patterns_video_call) &&
+                    !PhoneNumberUtils.isVideoCallNumValid(number)) {
                     Toast.makeText(mContext,
                             R.string.toast_make_video_call_failed, Toast.LENGTH_LONG).show();
                     return true;
